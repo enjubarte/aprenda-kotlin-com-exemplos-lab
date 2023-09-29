3636e291a11ca0da57364f3ab901236ba1b44a4b
Douglas Costa <78611411+enjubarte@users.noreply.github.com>
2023-09-29 15:02:40 -0300

    Mudando o nível de difícil para Avançado e usando como atributo de ConteudoEducacional e Formação

--- desafio.kt
+++ desafio.kt
@@ -1,12 +1,12 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-class Usuario
+data class Usuario(val nome: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, var duracao: Int = 60, val nivel: Nivel)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>, val nivel: Nivel) {
 
     val inscritos = mutableListOf<Usuario>()
     
